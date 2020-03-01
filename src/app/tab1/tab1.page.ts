3bf6e6713ae197659c3cc6f73d08bd10022060c9
yongseok <iyongseok@iyongseog-ui-MacBookPro.local>
2020-03-01 21:09:38 +0900

    add ios/android & add camera plugin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraSource, CameraResultType } from '@capacitor/core'
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor() {}
+    constructor(private sanitizer: DomSanitizer) { }
+    photo: any;
+
+    async takePicture() {
+        const takePhoto = await Plugins.Camera.getPhoto({
+            quality: 90,
+            resultType: CameraResultType.DataUrl,
+            source: CameraSource.Camera
+        });
+
+        this.photo = this.sanitizer.bypassSecurityTrustUrl(takePhoto && (takePhoto.dataUrl))
+    }
+
 
 }
